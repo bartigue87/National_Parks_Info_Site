518f1b697426ec4a62771301ab7a6c9d7926a313
Brandon Artigue <bartigue87@yahoo.com>
2023-03-03 17:34:19 -0700

    parkpage styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ParkPage from "./pages/ParkPage";
 
 function App() {
-  let routes = (<Routes>
-    <Route exact path="/" element={<Homepage />} />
-    <Route path="/:pid" element={<ParkPage />} />
-</Routes>)
+  let routes = (
+    <Routes>
+      <Route exact path="/" element={<Homepage />} />
+      <Route path="/:pid" element={<ParkPage />} />
+    </Routes>
+  );
 
   return (
     <div className="App">
--- client/src/pages/ParkPage.js
+++ client/src/pages/ParkPage.js
@@ -1,5 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+import "./ParkPage.css";
+import backArrow from "../images/back-arrow.png";
 
 export default function ParkPage() {
-  return <div></div>;
+  const [parkData, setParkData] = useState();
+  const navigate = useNavigate();
+
+  const parkId = useParams().pid;
+  const API_BASE = `http://localhost:3000/Parks/${parkId}`;
+
+  useEffect(() => {
+    fetch(API_BASE)
+      .then((response) => response.json())
+      .then((data) => setParkData(data));
+  }, [API_BASE]);
+
+  let parkImage = [];
+  let thingsToDo = [];
+
+  if (parkData) {
+    parkImage = parkData.imgUrl.map((img) => {
+      return (
+        <div
+          className="park-image"
+          style={{ backgroundImage: `url(${img})` }}
+        />
+      );
+    });
+  }
+  if (parkData) {
+    thingsToDo = parkData.todo.map((todo) => {
+      return <li className="todo">{todo}</li>;
+    });
+  }
+
+  function returnHome(event) {
+    navigate(`/`, { replace: true });
+  }
+
+  return (
+    <div className={`single-park-container ${parkData.type}-main-bg`}>
+      <div className={`back-btn ${parkData.type}-sec-bg`} onClick={returnHome}>
+        <img src={backArrow} alt="back arrow" />
+        <p>Back</p>
+      </div>
+      {!parkData ? (
+        <div>Loading.....</div>
+      ) : (
+        <div>
+          <h1 className="title">{`${parkData.name} National Park`}</h1>
+          <div className="img-container">{parkImage}</div>
+          <div className="info-todo-container">
+            <div className="info-container">
+              <p>
+                <strong>Location:</strong> {parkData.location}
+              </p>
+              <p>
+                <strong>Date Established:</strong> {parkData.dateEstablished}
+              </p>
+              <p>{parkData.description}</p>
+            </div>
+            <ol>Things to do: {thingsToDo}</ol>
+          </div>
+          <div className={`sidenote ${parkData.type}-sec-bg`}>
+            <p>{`America's ${parkData.rank} National Park`}</p>
+            <p>
+              <strong>Did You Know?:</strong>
+              <br></br> {parkData.funFact}
+            </p>
+            <p>
+              {`Animal to look for: `}
+              <span>{parkData.animal}</span>
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
