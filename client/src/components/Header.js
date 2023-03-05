bfa61df24ad34147b4bea8e44a5621a771d2c5ee
Brandon Artigue <bartigue87@yahoo.com>
2023-03-04 23:17:31 -0700

    remove map because mobile issues

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
 export default function Header() {
-  let navigate = useNavigate();
-
-  function openMap(event) {
-    navigate(`/map`, { replace: true });
-  }
   return (
     <div className="header-container">
       <h1>National Park Info Site</h1>
-      <button className="game-btn" onClick={openMap}>
-        View Map
-      </button>
     </div>
   );
 }
