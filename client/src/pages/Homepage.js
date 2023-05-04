16ef9a0b44845d1f917b7de2a8aa43b2c083b795
Brandon Artigue <bartigue87@yahoo.com>
2023-05-04 11:29:27 -0600

    add loading while parks are loading

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -7,6 +7,7 @@ import "./Homepage.css";
 export default function Homepage() {
   const [parksData, setParksData] = useState([]);
   const [filteredParkList, setFilteredParkList] = useState("");
+  const [loading, setLoading] = useState(true);
   const [query, setQuery] = useState("");
 
   const API_BASE = `${process.env.REACT_APP_BACKEND_URL}/parks`;
@@ -17,14 +18,19 @@ export default function Homepage() {
       .then((res) => res.json())
       .then((data) => setParksData(data))
       .catch((err) => console.error("Error: ", err));
+    setLoading(false);
   }, [API_BASE]);
 
-  const parks = parksData
-    .filter((park) => park.location.toUpperCase().includes(filteredParkList))
-    .filter((park) => park.name.toLowerCase().includes(query))
-    .map((park) => {
-      return <Park key={park._id} {...park} />;
-    });
+  const parks = loading ? (
+    <h1>Loading Parks...</h1>
+  ) : (
+    parksData
+      .filter((park) => park.location.toUpperCase().includes(filteredParkList))
+      .filter((park) => park.name.toLowerCase().includes(query))
+      .map((park) => {
+        return <Park key={park._id} {...park} />;
+      })
+  );
 
   function checkLocation(location) {
     setFilteredParkList(location);
