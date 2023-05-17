afe7b1e7a79c735d11c9854ccb90ee21ea97eb4f
Brandon Artigue <bartigue87@yahoo.com>
2023-05-17 14:12:16 -0600

    remove console logs add animation during loading

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -9,10 +9,10 @@ export default function Homepage() {
   const [parksData, setParksData] = useState([]);
   const [filteredParkList, setFilteredParkList] = useState("");
   const [loading, setLoading] = useState(true);
+  const [initialScreen, setInitialScreen] = useState(false);
   const [query, setQuery] = useState("");
 
-  const API_BASE = `${process.env.REACT_APP_BACKEND_URL}/parks`;
-  console.log(API_BASE);
+  const API_BASE = `${process.env.REACT_APP_BACKEND_URL}/parks?only=name`;
 
   const fetchParks = async () => {
     try {
@@ -30,7 +30,17 @@ export default function Homepage() {
   }, []);
 
   const parks = loading ? (
-    <h1 style={{ color: "Black", margin: "0 auto" }}>Loading Parks...</h1>
+    <div>
+      <h1 style={{ color: "Black", margin: "0 auto" }}>Loading Parks...</h1>
+      <lottie-player
+        src={"https://assets9.lottiefiles.com/packages/lf20_SwKPCDD0fy.json"}
+        background="transparent"
+        speed={1}
+        style={{ width: "90%", height: "300px" }}
+        loop
+        autoplay
+      ></lottie-player>
+    </div>
   ) : (
     parksData
       .filter((park) => park.location.toUpperCase().includes(filteredParkList))
